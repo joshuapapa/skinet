3548fadf1519335e7fd7a6d846a044acf021721b
Joshua David Papa <jdpapa0821@ymail.com>
2022-10-02 21:26:51 +0800

    End of section 13

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -15,7 +15,7 @@ namespace API.Extensions
 
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
-
+            services.AddScoped<IBasketRepository, BasketRepository>();
             services.Configure<ApiBehaviorOptions>(options => {
                 options.InvalidModelStateResponseFactory = ActionContext => {
                     var errors = ActionContext.ModelState
